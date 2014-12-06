2ea9c67300a34cc6345e17f91b4f31fff7b27c87
MiguelJimenez <maj009@gmail.com>
2014-12-06 10:48:31 +0100

    20141206 Libreria
    Fin de la clase 4

---
+++ 03SubirArchivos/enviar-archivo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Subir Archivos al servidor con PHP</title>
+</head>
+<body>
+	<form name="enviar_archivo_frm" method="post" action="subir-archivo.php" enctype="multipart/form-data">
+		<input type="file" name="archivo_fls">
+		<input type="submit" name="subir_btn" value="Subir Archivo">
+		
+	</form>	
+	
+</body>
+</html>
